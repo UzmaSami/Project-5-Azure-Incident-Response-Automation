6be4cdaa9b6649df9d9a03c8ee15906c4851e2dc
Uzma Shabbir <uzma.khan22871@gmail.com>
2026-05-12 22:31:47 +0100

    Update 02-check-uploaded-watchlist.ps1

--- 05-watchlists/02-check-uploaded-watchlist.ps1
+++ 05-watchlists/02-check-uploaded-watchlist.ps1
@@ -1,1 +1,6 @@
+# This asks for ALL watchlists in the workspace                
+PS /home/uzma> Get-AzResource -ResourceGroupName $rg `
+>>     -ResourceType "Microsoft.OperationalInsights/workspaces/providers/watchlists" `
+>>     -ResourceName "$ws/Microsoft.SecurityInsights" `
+>>     -ApiVersion "2023-02-01"
 
